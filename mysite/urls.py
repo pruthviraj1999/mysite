e5784c91d0ed2f322d5385a9ae68d13d2f602c47
cherukuakhila25 <cherukuakhila25@gmail.com>
2023-05-19 11:16:11 +0530

    migration and creation of super user is done

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,4 +1,4 @@
-"""proj1 URL Configuration
+"""mysite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
